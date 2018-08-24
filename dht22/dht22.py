823ea0246f8be67875bc1d82c8d2adf7a16d04e6
rkarlsba <roy@karlsbakk.net>
2018-08-24 15:51:14 +0200

    lala

--- dht22/dht22.py
+++ dht22/dht22.py
@@ -5,16 +5,18 @@ import machine
 # setup the dht22
 sensor = dht.DHT22(machine.Pin(16))
 
-sensor.measure()
-temp = sensor.temperature()
-hum = sensor.humidity()
+#sensor.measure()
 
 def gettemp(with_header=1):
+    sensor.measure()
+    temp = sensor.temperature()
     if with_header:
         print("Temperature = ", end='')
     print(temp)
 
 def gethum(with_header=1):
+    sensor.measure()
+    hum = sensor.humidity()
     if with_header:
         print("Humidity = ", end='')
     print(hum)
