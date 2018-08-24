847164c9f0f5e351421bca6706e4b6fa89ea230b
rkarlsba <roy@karlsbakk.net>
2018-08-24 17:45:10 +0200

    hm

--- dht22/dhtsock.py
+++ dht22/dhtsock.py
@@ -1,45 +1,51 @@
 import network
 import sys
 import socket
+import machine
+import time
 
-wifi_ssid='roysittnett'
-wifi_password='ikkeveldighemmelig'
+def temphumsrv():
+    machine.freq(160000000)
 
-sta_if = network.WLAN(network.STA_IF)
-sta_if.active(True)
+    wifi_ssid='roysittnett'
+    wifi_password='ikkeveldighemmelig'
 
-ap_if = network.WLAN(network.AP_IF)
-ap_if.active(False)
+    sta_if = network.WLAN(network.STA_IF)
+    sta_if.active(True)
 
-sta_if.connect(wifi_ssid, wifi_password)
+    ap_if = network.WLAN(network.AP_IF)
+    ap_if.active(False)
 
-isconnected=0
+    sta_if.connect(wifi_ssid, wifi_password)
 
-for attempt in range(10):
-    if sta_if.isconnected():
-        isconnected=1
-        break
+    isconnected=0
 
-if isconnected == 0:
-    print("Failed to connect to wifi")
-    sys.exit(1)
-
-addr = socket.getaddrinfo('0.0.0.0', 5050)[0][-1]
-s = socket.socket()
-s.bind(addr)
-s.listen(1)
-print('listening on', addr)
-
-while True:
-    cl, addr = s.accept()
-    print('client connected from', addr)
-    cl_file = cl.makefile('rwb', 0)
-    while True:
-        line = cl_file.readline()
-        if not line or line == b'\r\n':
+    for attempt in range(10):
+        if sta_if.isconnected():
+            isconnected=1
+            time.sleep_ms(250)
             break
-    rows = ['<tr><td>%s</td><td>%d</td></tr>' % (str(p), p.value()) for p in pins]
-    response = html % '\n'.join(rows)
-    cl.send(response)
-    cl.close()
+
+    if isconnected == 0:
+        print("Failed to connect to wifi")
+        sys.exit(1)
+
+    addr = socket.getaddrinfo('0.0.0.0', 5050)[0][-1]
+    s = socket.socket()
+    s.bind(addr)
+    s.listen(1)
+    print('listening on', addr)
+
+    while True:
+        cl, addr = s.accept()
+        print('client connected from', addr)
+        cl_file = cl.makefile('rwb', 0)
+        while True:
+            line = cl_file.readline()
+            if not line or line == b'\r\n':
+                break
+        rows = ['<tr><td>%s</td><td>%d</td></tr>' % (str(p), p.value()) for p in pins]
+        response = html % '\n'.join(rows)
+        cl.send(response)
+        cl.close()
 
--- dht22/main.py
+++ dht22/main.py
@@ -1,1 +1,2 @@
 import dht22
+#import dhtsock
