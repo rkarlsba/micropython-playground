200e61ab8f6ef1c94ce653bc1d024e2439d85e26
Roy Sigurd Karlsbakk <roysk@hioa.no>
2018-11-17 16:04:24 +0100

    derr - sender til github foer skjermen kobles opp

--- dht22/globals_local.py
+++ dht22/globals_local.py
@@ -4,14 +4,14 @@
 use_networking = 0
 
 # Pinner
-pin_dht = 0     # D3
+pin_dht = 2     # D3
 pin_led_r = 12  # D6
 pin_led_g = 13  # D7
 pin_led_b = 15  # D8
 pin_led_o = 2   # onboard - logical low
 
-pin_sda = 4     # I2C Send DAta
-pin_scl = 5     # I2C Sync CLock
+pin_sda = 4     # D2 - I2C Send DAta
+pin_scl = 5     # D1 - I2C Sync CLock
 
 port = 4949
 listen_addr = '0.0.0.0'
